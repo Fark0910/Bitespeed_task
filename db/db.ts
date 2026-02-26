4fe93867422e42886bcb3e9d3262e9bd757ab344
Fark0910 <fardeen7781@gmail.com>
2026-02-27 03:14:14 +0530

    fix:ssl for railway mysql

--- db/db.ts
+++ db/db.ts
@@ -3,12 +3,12 @@ dotenv.config();
 import mysql,  { Pool, PoolOptions }  from "mysql2/promise";
 
 const pooloption:PoolOptions = {
-    host:  process.env.sqlDB_HOST,                                                                         
+    host: process.env.sqlDB_HOST,                                                                         
     user: process.env.sqlDB_USER, 
     password: process.env.sqlDB_PASS, 
     database: process.env.sqlDB_NAME,
     port:Number(process.env.sqlDB_port),
-    
+    ssl:{rejectUnauthorized:false},
   waitForConnections: true,
   connectionLimit: 10, 
   queueLimit: 0
